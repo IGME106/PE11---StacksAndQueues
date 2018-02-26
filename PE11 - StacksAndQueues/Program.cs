fe2cc30d4419050c3a730ec5e92fcee5ac149a65
bxk8027 <bxk8027@IGM2025-29>
2018-02-26 10:26:29 -0500

    Program complete

--- PE11 - StacksAndQueues/Program.cs
+++ PE11 - StacksAndQueues/Program.cs
@@ -10,17 +10,127 @@ namespace PE11___StacksAndQueues
     {
         static void Main(string[] args)
         {
+            GameStack myGameStack = new GameStack();
+
+            Console.WriteLine();
+
+            Console.WriteLine("The following spells are being put on the stack:");
+
+            Console.WriteLine("- Shock");
+            myGameStack.Push("Shock");
+            Console.WriteLine("- Fork");
+            myGameStack.Push("Fork");
+            Console.WriteLine("- CounterSpell");
+            myGameStack.Push("CounterSpell");
+            Console.WriteLine("- Force of Will");
+            myGameStack.Push("Force of Will");
+            Console.WriteLine("- Deflection");
+            myGameStack.Push("Deflection");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Spells resolving in reverse order:");
+
+            while (myGameStack.IsEmpty)
+            {
+                Console.WriteLine("- " + myGameStack.Pop());
+            }
+
+            GameQueue myGameQueue = new GameQueue();
+
+            Console.WriteLine();
+
+            Console.WriteLine("The following players are joining the queue:");
+
+            Console.WriteLine("- Bob");
+            myGameQueue.Enqueue("Bob");
+            Console.WriteLine("- Tom");
+            myGameQueue.Enqueue("Tom");
+            Console.WriteLine("- Sara");
+            myGameQueue.Enqueue("Sara");
+            Console.WriteLine("- Jim");
+            myGameQueue.Enqueue("Jim");
+            Console.WriteLine("- Larry");
+            myGameQueue.Enqueue("Larry");
+
+            Console.WriteLine();
+
+            while (myGameQueue.IsEmpty)
+            {
+                Console.Write("\"" + myGameQueue.Dequeue() + "\" has joined the server: ");
+                Console.Write("  " + myGameQueue.Count + " player(s) left in the queue\n");
+            }
+
+            Console.ReadKey();
         }
     }
 
-    class GameStack : IStack
+    public class GameStack : IStack
     {
+        private List<String> iStack = new List<String> ();
+        
+        public bool IsEmpty
+        {
+            get { return iStack.Any(); }
+        }
 
+        public int Count
+        {
+            get { return iStack.Count(); }
+        }
+
+        public void Push(String s)
+        {
+            iStack.Add(s);
+        }
+
+        public String Pop()
+        {
+            string returnValue = returnValue = iStack[Count - 1];
+
+            iStack.RemoveAt(Count - 1);
+
+            return returnValue;
+        }
+
+        public String Peek()
+        {
+            return iStack[Count - 1];
+        }
     }
 
     class GameQueue : IQueue
     {
+        private List<String> iStack = new List<String>();
 
+        public bool IsEmpty
+        {
+            get { return iStack.Any(); }
+        }
+
+        public int Count
+        {
+            get { return iStack.Count(); }
+        }
+
+        public void Enqueue(String s)
+        {
+            iStack.Add(s);
+        }
+
+        public String Dequeue()
+        {
+            string returnValue = returnValue = iStack[0];
+
+            iStack.RemoveAt(0);
+
+            return returnValue;
+        }
+
+        public String Peek()
+        {
+            return iStack[Count - 1];
+        }
     }
 
     interface IStack
